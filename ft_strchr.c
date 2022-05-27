2ce3652b001667ed1ac05fb08096b9cb73883d16
micberna <micberna42@gmail.com>
2022-05-26 21:37:45 -0300

    git6

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: micberna <micberna@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 22:42:59 by micberna          #+#    #+#             */
-/*   Updated: 2022/05/25 19:06:43 by micberna         ###   ########.fr       */
+/*   Updated: 2022/05/26 21:35:03 by micberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,46 @@
 // #include <string.h>
 // char *strchr(const char * s , int c ); 
 
-char	*ft_strchr(const char *s, int c);
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
 {
-	char *str;
+	char	*str;
+
 	str = (char *)s;
-   	while (*str != c);
+	while (*str != c)
+	{	
 		if (*str == '\0')
 		{
 			return (NULL);
 		}
 		str++;
-
+	}
 	return (str);
 }
+
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char str[] = "michelle";
+	char *result;
+	char *result2;
+	char *result3;
+	char *result4;
+	char nok;
+	char ok;
+
+	nok = 'z';
+	ok = 'l';
+
+	result = ft_strchr(str, nok);
+	result2 = strchr(str, nok);
+	result3 = ft_strchr(str, ok);
+	result4 = strchr(str, ok);
+
+	printf("%s - nok - minha funçao\n%s - nok - funçao original\n", result, result2);
+	printf("%s - ok - minha funçao\n%s - ok - funçao original\n", result3, result4);
+	return (0);
+}
+
--- test_strchr.c
+++ test_strchr.c
@@ -2,28 +2,30 @@
 #include "libft.h"
 #include<ctype.h>
 #include<string.h>
-char ft_strchr(const char *s, int c);
 
-int main(void)
+char *ft_strchr(const char *s, int c);
+
+
+int main()
 {
-	char  n[] = "michelle"
-	int s = ft;
-	a = '1';
-	float b;
-	b = 178;
-	char str
+	const char *str = "Michelle";
+	char  x;
+	x = 'e';
+	char y;
+	y = 'd';
 
-int result;
-int result2;
-int result3;
-int result4;
+	int result;
+	int result2;
+	int result3;
+	int result4;
 
-result = ft_isdigit(b,);
-result2 = isdigit(b);
-result3 = ft_isdigit(a);
-result4 = isdigit(a);
-	printf("minha funçao: %d funçao original: %d\n",result, result2);
-	printf("minha funçao: %d funçao original: %d\n",result3, result4);
+	result = *ft_strchr(str, x);
+	result2 = strchr(str, x);
+	result3 = *ft_strchr(str, y);
+	result4 = strchr(str, y);
+
+	printf("minha funçao: : %c funçao original %c\n", result, result2);
+	printf("minha funçao: : %c funçao original %c\n", result3, result4);
 
 return (1);
 }
