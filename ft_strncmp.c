217f764f3626e561e5d9d564f310b889cc4de2d1
micberna <micberna42@gmail.com>
2022-06-14 20:22:27 -0300

    tudo verde

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: micberna <micberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 20:27:34 by micberna          #+#    #+#             */
-/*   Updated: 2022/06/14 19:35:31 by micberna         ###   ########.fr       */
+/*   Updated: 2022/06/14 20:12:21 by micberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (((s1)[i] != '\0' || (s2)[i] != '\0') && i < n)
 	{
-		if ((s1)[i] != (s2)[i])
-			return ((s1)[i] - (s2)[i]);
+		if ((unsigned char)(s1)[i] != (unsigned char)(s2)[i])
+			return ((unsigned char)(s1)[i] - (unsigned char)(s2)[i]);
 		i++;
 	}
 	return (0);
